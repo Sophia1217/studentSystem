b9957734f7c3344461cb7ddd51fdce97538ccadb
lewxs <lewxs@qq.com>
2022-08-03 18:22:57 +0800

    fead(0000)：【学工系统】在籍学生信息页面编码

---
+++ src/views/components/checkboxCom/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="checkboxCom">
+    <el-checkbox class="elcheckbox" :indeterminate="objProp.isIndeterminate" 
+      v-model="objProp.checkAll" @change="handleCheckAllChange">全选</el-checkbox>
+    <el-checkbox-group v-model="objProp.choose" @change="handleCheckedCitiesChange">
+      <el-checkbox v-for="item in objProp.checkBox" :label="item.val" :key="item.val">{{item.label}}</el-checkbox>
+    </el-checkbox-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'checkboxCom',
+
+  data() {
+    return {
+      
+    };
+  },
+  props: {
+    objProp: {
+      type: Object,
+      default: ()=>{},
+    }
+  },
+
+  mounted() {},
+
+  methods: {
+    handleCheckAllChange(val) { 
+      this.$emit('training',val)
+    },
+    handleCheckedCitiesChange(val) {
+      this.$emit('checkedTraining',val)
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.checkboxCom{
+  display: flex;
+}
+.elcheckbox{
+  margin-right: 20px;
+}
+</style>
--- src/views/student/absentee/index.vue
+++ src/views/student/absentee/index.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="StudentSysIndex">
-    在籍学生信息
+    <el-tabs v-model="activeName" class="tabs" @tab-click="handleClick">
+      <el-tab-pane label="可管理学生" name="first">
+        <span slot="label"><i class="icon tabsicon_ke"></i>可管理学生</span>
+      </el-tab-pane>
+      <el-tab-pane label="全校学生查询" name="second">
+        <span slot="label"><i class="icon tabsicon_quan"></i>全校学生查询</span>
+      </el-tab-pane>
+    </el-tabs>
+    <man-student v-if="activeName == 'first'"></man-student>
+    <all-student v-if="activeName == 'second'"></all-student>
   </div>
 </template>
 
 <script>
+import ManStudent from './manStudent/index.vue'
+import AllStudent from './allStudent/index.vue'
 export default {
-  name: 'StudentSysIndex',
-
+  name: 'absentee',
+  components:{ ManStudent, AllStudent },
   data() {
     return {
-      
+      activeName: 'first'
     };
   },
 
@@ -19,13 +30,36 @@ export default {
   },
 
   methods: {
-    
-  },
+    handleClick(tab, event) {
+      this.activeName = tab.name
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .StudentSysIndex{
   padding:20px;
+  // background: #fff;
+  .tabs{
+    .icon{
+      display: inline-block;
+      width:20px;
+      height: 20px;
+      margin-right: 8px;
+    }
+    .tabsicon_ke{
+      background: url('../../../assets/images/icon_ke.png');
+      background-repeat: no-repeat;
+      background-position: center;
+      vertical-align: middle;
+    }
+    .tabsicon_quan{
+      background: url('../../../assets/images/icon_quan.png');
+      background-repeat: no-repeat;
+      background-position: center;
+      vertical-align: middle;
+    }
+  }
 }
 </style>

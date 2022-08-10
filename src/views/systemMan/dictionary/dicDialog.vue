7fd1d634fe46fee6955821ec849ed9fbb6d143cc
yifuzhiming227 <792885303@qq.com>
2022-08-10 15:54:20 +0800

    字典fix

--- src/views/systemMan/dictionary/dicDialog.vue
+++ src/views/systemMan/dictionary/dicDialog.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="dicDialog">
-    <el-dialog :title="rowId?'编辑字典':'新增字典'" :visible.sync="dialogVisible" width="30%"
-      :before-close="handleCloseDia">
-      <el-form :model="queryParams" ref="queryParams" class="queryForm" size="small" label-width="68px">
+    <el-dialog
+      :title="rowId ? '编辑字典' : '新增字典'"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleCloseDia"
+    >
+      <el-form
+        :model="queryParams"
+        ref="queryParams"
+        class="queryForm"
+        size="small"
+        label-width="68px"
+      >
         <el-form-item label="字典名称" prop="roleName">
-          <el-input v-model="queryParams.roleName" size="small" placeholder="请输入字典名称"></el-input>
+          <el-input
+            v-model="queryParams.codeTableEnglish"
+            size="small"
+            placeholder="请输入字典名称"
+          ></el-input>
         </el-form-item>
         <el-form-item label="字典类型" prop="roleDic">
-          <el-input v-model="queryParams.roleDic" size="small" placeholder="请输入字典类型"></el-input>
+          <el-input
+            v-model="queryParams.codeTableChinese"
+            size="small"
+            placeholder="请输入字典类型"
+          ></el-input>
         </el-form-item>
         <el-form-item label="字典状态" prop="roleState">
-          <el-switch v-model="queryParams.roleState" active-color="#23AD6F" inactive-color="#E0E0E0" ></el-switch>
+          <el-switch
+            v-model="queryParams.state"
+            active-color="#23AD6F"
+            inactive-color="#E0E0E0"
+          ></el-switch>
         </el-form-item>
         <el-form-item label="备注" prop="textarea">
-          <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="queryParams.textarea"></el-input>
+          <el-input
+            type="textarea"
+            :rows="2"
+            placeholder="请输入内容"
+            v-model="queryParams.remark"
+          ></el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
@@ -26,45 +53,45 @@
 
 <script>
 export default {
-  name: 'dicDialog',
+  name: "dicDialog",
 
   data() {
     return {
       queryParams: {
-        roleName: '',
-        roleState:'',
-        roleDic: '',
-        textarea:''
-      }
+        codeTableEnglish: "", //字典类型
+        codeTableChinese: "", //字典名称
+        state: "", //状态
+        remark: "", //备注
+      },
     };
   },
   props: {
     dialogVisible: {
       type: Boolean,
-      default:false
+      default: false,
     },
-    rowId: { // id存在是编辑否则是新建
+    rowId: {
+      // id存在是编辑否则是新建
       type: String | Number,
-      default:''
-    }
+      default: "",
+    },
   },
   mounted() {
-    console.log(this.rowId)
+    console.log(this.rowId);
   },
 
   methods: {
     handleCloseDia() {
-      this.$emit('handleCloseDia')
+      this.$emit("handleCloseDia");
     },
     handleCommitDia() {
-      this.$emit('handleCommitDia');
-    }
+      this.$emit("handleCommitDia", this.queryParams);
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.dicDialog{
-
+.dicDialog {
 }
 </style>

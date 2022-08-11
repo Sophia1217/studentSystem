a68d8a37d7526e39017664f323dd76fbfa291624
zhangchun <443986991@qq.com>
2022-08-11 07:58:00 +0000

    政工任免班主任更新.vue

--- src/views/politicalwork/teacherappoint/index.vue
+++ src/views/politicalwork/teacherappoint/index.vue
@@ -4,8 +4,8 @@
       <el-tab-pane label="在岗" name="first">
         <span slot="label"><i class="icon tabsicon_on" />在岗</span>
       </el-tab-pane>
-      <el-tab-pane label="不在岗" name="second">
-        <span slot="label"><i class="icon tabsicon_off" />不在岗</span>
+      <el-tab-pane label="非在岗" name="second">
+        <span slot="label"><i class="icon tabsicon_off" />非在岗</span>
       </el-tab-pane>
     </el-tabs>
     <OnDuty v-if="activeName == 'first'" />
@@ -17,7 +17,7 @@
 import OnDuty from './onDuty/index.vue'
 import OffDuty from './offDuty/index.vue'
 export default {
-  name: 'AssistantAppoint',
+  name: 'Teacherappoint',
   components: { OnDuty, OffDuty },
   data() {
     return {
@@ -51,13 +51,13 @@ export default {
       margin-right: 8px;
     }
     .tabsicon_on{
-      background: url('../../../assets/images/icon_onDuty.png');
+      background: url('~@/assets/images/icon_onDuty.png');
       background-repeat: no-repeat;
       background-position: center;
       vertical-align: middle;
     }
     .tabsicon_off{
-      background: url('../../../assets/images/icon_offDuty.png');
+      background: url('~@/assets/images/icon_offDuty.png');
       background-repeat: no-repeat;
       background-position: center;
       vertical-align: middle;

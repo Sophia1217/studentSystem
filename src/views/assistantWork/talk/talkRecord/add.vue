eecabe8064d510912400d1bdccd7ab8d4ff10021
luosheng <792885303@qq.com>
2022-09-21 08:33:49 +0800

    demo

---
+++ src/views/assistantWork/talk/talkRecord/add.vue
@@ -0,0 +1,366 @@
+<template>
+  <div class="addRole">
+    <div class="permissions">
+      <div>
+        <span class="title">谈话对象</span>
+      </div>
+      <div class="wai-container">
+        <div class="roleWrap" v-for="(role, index) in renshu" :key="index">
+          <div class="roleStyle">
+            <div class="name">{{ index + 1 }}:</div>
+            <div>
+              <el-select
+                v-model="role.value"
+                @change="hold"
+                size="small"
+                placeholder="请选择"
+              >
+                <el-option
+                  v-for="(item, index) in stuData"
+                  :key="index"
+                  :label="item.label"
+                  :value="item.value"
+                >
+                </el-option>
+              </el-select>
+            </div>
+          </div>
+          <div
+            v-if="index == renshu.length - 1"
+            class="editBtn"
+            @click="addRoles(role, index)"
+          >
+            <i class="addIcon"></i>
+          </div>
+          <div v-else class="deleIcon" @click="deleRoles(role, index)">
+            <i></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="permissions">
+      <div>
+        <span class="title">谈话内容</span>
+      </div>
+      <el-form ref="form" label-width="80px">
+        <el-row :gutter="20">
+          <el-col :span="6">
+            <el-form-item label="谈话主题">
+              <el-input v-model="name" placeholder="请输入"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-tag
+              v-for="tag in tags"
+              :key="tag.name"
+              closable
+              :type="tag.type"
+            >
+              {{ tag.name }}
+            </el-tag>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+
+    <!-- <div class="editBottom">
+      <div class="btn cancel" @click="cancel()">
+        <i class="icon noIcon"></i> 取消
+      </div>
+      <div class="btn confirm" @click="sava()">
+        <i class="icon yesIcon"></i> 提交
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+import {
+  queryTreeList,
+  queryTreeListJ,
+  savaTreeList,
+  savaEditList,
+} from "@/api/systemMan/role";
+export default {
+  data() {
+    return {
+      tags: [
+        { name: "标签一", type: "" },
+        { name: "标签二", type: "success" },
+        { name: "标签三", type: "info" },
+        { name: "标签四", type: "warning" },
+        { name: "标签五", type: "danger" },
+        { name: "标签一", type: "" },
+        { name: "标签二", type: "success" },
+        { name: "标签三", type: "info" },
+        { name: "标签四", type: "warning" },
+        { name: "标签五", type: "danger" },
+      ],
+      renshu: ["", "", "", "", "", "", "", "", "", ""],
+      stuData: [
+        {
+          value: "选项1",
+          label: "黄金糕",
+        },
+        {
+          value: "选项2",
+          label: "双皮奶",
+          disabled: true,
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎",
+        },
+        {
+          value: "选项4",
+          label: "龙须面",
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭",
+        },
+      ],
+      modId: "",
+      // 查询参数
+      queryParams: {
+        roleRem: "",
+        roleName: this.isEdit == "1" ? "" : this.$route.query.roleNameEdit, // 编辑是2
+      },
+      treeData: [],
+      defaultProps: {
+        children: "children",
+        label: "title",
+      },
+      isEdit: "",
+      savaData: [], //新增提交所需要的menuList
+      roleId1: "", ////编辑请求的id,
+      arr: [],
+      arr1: ["01"],
+    };
+  },
+
+  mounted() {
+    this.isEdit = this.$route.query.isEdit;
+    this.roleId1 = this.$route.query?.UpId;
+    this.roleName1 = this.$route.query?.roleNameEdit;
+    this.queryParams.roleRem = this.$route.query?.rem;
+    this.handleTree();
+  },
+
+  methods: {
+    cancel() {
+      window.history.go(-1);
+    },
+    handleTree() {
+      if (this.isEdit == 2) {
+        let data = { roleId: this.roleId1 };
+        let dataALL = { roleId: this.$store.state.user.roleId };
+        queryTreeListJ(data)
+          .then((res) => {
+            var result = res.data;
+            this.arr = result;
+            // this.getData(result); //
+          })
+          .catch((err) => {});
+        queryTreeList(dataALL)
+          .then((res) => {
+            this.treeData = res.data;
+            this.setkeys();
+          })
+          .catch((err) => {});
+      } else {
+        let data = { roleId: this.$store.state.user.roleId };
+        queryTreeList(data)
+          .then((res) => {
+            this.treeData = res.data;
+          })
+          .catch((err) => {});
+      }
+    },
+    setkeys() {
+      this.$refs.tree.setCheckedKeys(this.arr);
+    },
+    getData(data) {
+      for (var i in data) {
+        this.arr.push(data[i].modId); //将第一层的保存出来，
+        if (data[i].children) {
+          // if(data[i].length >)
+          this.getData(data[i].children);
+        }
+      }
+      return this.arr;
+    },
+    //elementUi中自带的方法，可以获取到所有选中的节点
+    currentChecked(nodeObj, SelectedObj) {
+      var that = this;
+      const { checkedNodes, halfCheckedKeys } = SelectedObj;
+      var menuList = checkedNodes.map((item) => item.modId);
+      that.savaData = menuList.concat(halfCheckedKeys); //要获取上级根节点
+    },
+    sava() {
+      if (this.isEdit === "1") {
+        let data = {
+          userId: this.$store.getters.userId,
+          menuList: this.savaData,
+          roleName: this.queryParams.roleName,
+          loginRoleId: this.$store.getters.roleId,
+          roleRem: this.queryParams.roleRem,
+        };
+        savaTreeList(data)
+          .then(() => {
+            this.$router.push({
+              path: "/systems/role",
+            });
+          })
+          .catch((err) => {});
+      } else {
+        let data = {
+          userId: this.$store.getters.userId,
+          menuList: this.savaData.length > 0 ? this.savaData : this.arr, //如果用户进来没编辑，默认前一次筛选出来的树
+          roleName: this.queryParams.roleName,
+          roleId: this.roleId1,
+          roleRem: this.queryParams.roleRem,
+        };
+        savaEditList(data)
+          .then(() => {
+            this.$router.push({
+              path: "/systems/role",
+            });
+          })
+          .catch((err) => {});
+      }
+    },
+    savaEditList() {},
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.addRole {
+  .editBtn {
+    padding: 0 10px;
+    margin-left: 22px;
+    border-radius: 4px;
+    font-weight: 400;
+    font-size: 14px;
+    color: #005657;
+    cursor: pointer;
+    line-height: 32px;
+    .addIcon {
+      display: inline-block;
+      width: 15px;
+      height: 15px;
+      background: url("~@/assets/images/addicon.png") no-repeat center;
+    }
+  }
+  .greenIcon {
+    margin-top: 10px;
+    background: url("~@/assets/assistantPng/add.png") no-repeat;
+  }
+  .roleWrap {
+    background: #fff;
+    margin-top: 20px;
+
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    .roleStyle {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      font-size: 14px;
+      color: #1f1f1f;
+      .name {
+        margin-right: 20px;
+        font-weight: 600;
+      }
+      .treeStyle {
+        font-weight: 400;
+        margin-left: 30px;
+      }
+    }
+  }
+  .deleIcon {
+    margin-left: 30px;
+    cursor: pointer;
+    i {
+      display: inline-block;
+      vertical-align: middle;
+      width: 20px;
+      height: 20px;
+      background: url("~@/assets/images/detelIcon.png");
+    }
+  }
+  .wai-container {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+  }
+  .permissions {
+    margin-top: 20px;
+    flex-direction: row;
+    background: #fff;
+    padding: 20px;
+    // height: calc(100vh - 250px);
+    .title {
+      font-weight: 600;
+      font-size: 20px;
+      color: #1f1f1f;
+      line-height: 28px;
+    }
+    .perName {
+      font-weight: 600;
+      font-size: 14px;
+      color: #1f1f1f;
+      line-height: 28px;
+      margin-right: 20px;
+    }
+  }
+  .editBottom {
+    width: 100%;
+    height: 60px;
+    background: #fff;
+    box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.1), 0 -2px 6px -1px rgba(0, 0, 0, 0.2);
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 100;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    .btn {
+      width: 84px;
+      height: 36px;
+      line-height: 36px;
+      text-align: center;
+      cursor: pointer;
+      border: 1px solid #005657;
+      border-radius: 2px;
+      margin-right: 20px;
+      .icon {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        vertical-align: middle;
+        margin-right: 5px;
+      }
+      .noIcon {
+        background: url("~@/assets/images/no.png");
+      }
+      .yesIcon {
+        background: url("~@/assets/images/yesW.png");
+      }
+    }
+    .cancel {
+      color: #005657;
+    }
+    .confirm {
+      background: #005657;
+      color: #fff;
+    }
+    .shishi {
+      width: 10px;
+    }
+  }
+}
+</style>

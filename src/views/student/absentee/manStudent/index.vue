26568b7d1302771d2a321940392a41dd5220d3ec
luosheng <792885303@qq.com>
2022-11-15 17:14:52 +0800

    成长档案审核上一条下一条以及各种校验

--- src/views/student/absentee/manStudent/index.vue
+++ src/views/student/absentee/manStudent/index.vue
@@ -528,14 +528,12 @@ export default {
       },
       dynamicModal: false,
       tableHeader: [
-        { dm: "zzmmc", mc: "政治面貌" },
-        { dm: "mzmc", mc: "民族" },
         { dm: "dwmc", mc: "学院" },
         { dm: "zydmc", mc: "专业" },
-        { dm: "xxxs", mc: "学制" },
+        { dm: "xxxs", mc: "学习形式" },
         { dm: "nj", mc: "年级" },
         { dm: "pyccmc", mc: "培养层次" },
-        { dm: "xz", mc: "学制(年)" },
+        { dm: "xz", mc: "学制" },
         { dm: "xjztmc", mc: "学籍状态" },
       ],
       dynamicsCheckboxs: {

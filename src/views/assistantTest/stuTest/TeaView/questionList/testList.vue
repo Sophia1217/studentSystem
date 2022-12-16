0897c791c46b8e3000fc9c43c7cf83c10305e76f
mrh <1156656381@qq.com>
2022-12-16 16:28:40 +0800

    改页面

--- src/views/assistantTest/stuTest/TeaView/questionList/testList.vue
+++ src/views/assistantTest/stuTest/TeaView/questionList/testList.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="testList">
+    <div class="header">
+      <div class="headerRight">问卷列表</div>
+      <div class="headerLeft">
+        <div class="btns borderGreen" @click="createTest">
+          <span class="title1">创建问卷</span>
+        </div>
+      </div>
+    </div>
     <div class="item">
       <div class="title">
-        <div class="titleRight">研究生2022测评问卷</div>
+        <div class="titleRight">问卷名称</div>
         <div class="titleLeft">
-          <div
-            class="btns borderGreen"
-            @click="doTest"
-            v-show="isFinish == '1'"
-          >
-            <span class="title1">填写问卷</span>
+          <div class="btns borderWhite">
+            <span class="title2">分析&amp;下载</span>
           </div>
-          <div class="btns borderWhite" v-show="isFinish == '2'">
-            <span class="title2">已填写</span>
+          <div class="btns borderWhite">
+            <span class="title2">预览</span>
+          </div>
+          <div class="btns borderWhite">
+            <span class="title2">复制</span>
+          </div>
+          <div class="btns borderWhite">
+            <span class="title2">删除</span>
+          </div>
+          <div class="btns borderGreen">
+            <span class="title1">编辑</span>
           </div>
         </div>
       </div>
@@ -23,7 +36,31 @@
           }}
           评测辅导员：{{ data }}
         </div>
-        <div class="contentLeft">问卷起始时间：{{ data }}至{{ data }}</div>
+        <div class="contentLeft">
+          <span>问卷起始时间:</span>
+
+          <el-date-picker
+            v-model="sqkfsj"
+            type="datetimerange"
+            range-separator="至"
+            start-placeholder="起始年月日"
+            end-placeholder="结束年月日"
+            value-format="yyyy-MM-dd HH:mm:ss"
+            @change="timeChange()"
+            :disabled="kgzt == '0' ? false : true"
+            :clearable="false"
+          >
+          </el-date-picker>
+
+          <el-switch
+            v-model="kgzt"
+            active-color="#23AD6F"
+            inactive-color="#E0E0E0"
+            active-value="0"
+            inactive-value="1"
+            @change="kgChange()"
+          ></el-switch>
+        </div>
       </div>
     </div>
   </div>
@@ -38,7 +75,8 @@ export default {
   data() {
     return {
       data: "1111",
-      isFinish: "1",
+      kgzt: "1",
+      sqkfsj: [],
     };
   },
   computed: {},
@@ -51,15 +89,14 @@ export default {
   },
 
   methods: {
-    doTest() {
-      this.$router.push({
-        path: "/fdyTestDetail",
-        // query: {
-        //   id: row.id,
-        //   xzdmOld: row.xzdm,
-        // },
-      });
+    createTest() {
+      //   this.$router.push({
+      //     path: "/fdyTestDetail",
+      //     // query: {}
+      //   });
     },
+    kgChange() {},
+    timeChange() {},
   },
 };
 </script>
@@ -67,28 +104,61 @@ export default {
 <style lang="scss" scoped>
 .testList {
   padding: 30px;
-  background: #f5f5f5;
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    // margin: 10px 10px 10px 10px;
+    margin-bottom: 5px;
+    .headerRight {
+      font-size: 22px;
+      font-weight: 600;
+    }
+    .headerLeft {
+      .btns {
+        cursor: pointer;
+        border-radius: 4px;
+        .title1 {
+          font-size: 16px;
+          text-align: center;
+          line-height: 32px;
+          color: #fff;
+          padding: 3px;
+          // vertical-align: middle;
+        }
+      }
+      .borderGreen {
+        border: 1px solid grey;
+        background: #005657;
+        display: flex;
+        align-items: center;
+      }
+    }
+  }
   .item {
     background: #ffffff;
-    height: 150px;
-    padding: 20px;
+    height: 120px;
+    padding: 10px;
     .title {
       display: flex;
       justify-content: space-between;
-      margin: 10px 10px 30px 10px;
+      margin: 10px 10px 20px 10px;
       .titleRight {
-        font-size: 26px;
-        font-weight: 600;
+        font-size: 20px;
+        font-weight: 500;
       }
       .titleLeft {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
         .btns {
           cursor: pointer;
-          border-radius: 4px;
+          border-radius: 2px;
           .title2 {
             font-size: 16px;
             text-align: center;
             line-height: 32px;
-            padding: 5px;
+            padding: 3px 15px 3px 15px;
             // vertical-align: middle;
           }
           .title1 {
@@ -96,7 +166,7 @@ export default {
             text-align: center;
             line-height: 32px;
             color: #fff;
-            padding: 5px;
+            padding: 3px 15px 3px 15px;
             // vertical-align: middle;
           }
         }
@@ -105,12 +175,14 @@ export default {
           background: #005657;
           display: flex;
           align-items: center;
+          margin-right: 10px;
         }
         .borderWhite {
           background: #fff;
           border: 1px solid grey;
           display: flex;
           align-items: center;
+          margin-right: 10px;
         }
       }
     }
@@ -121,6 +193,8 @@ export default {
       .contentRight {
         font-size: 16px;
         font-weight: 400;
+      }
+      .contentLeft {
       }
     }
   }

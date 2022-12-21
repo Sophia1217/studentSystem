cccabe1a3e9a891ab6c5654ca18da7466d58d8ca
luosheng <792885303@qq.com>
2022-12-21 09:32:17 +0800

    api

---
+++ src/api/test/testSetting.js
@@ -0,0 +1,34 @@
+import request from "@/utils/request.js"
+// 列表查询
+export function queryList(query) {
+    return request({
+        url: '/fdycpTm/queryFdycpTmList',
+        method: 'post',
+        data: query
+    })
+}
+
+
+export function delTest(query) {
+    return request({
+        url: '/fdycpTm/deleteFdycpTm',
+        method: 'post',
+        data: query
+    })
+}
+
+export function getDetail(query) {
+    return request({
+        url: '/fdycpTm/queryFdycpTmById',
+        method: 'post',
+        data: query
+    })
+}
+
+
+
+
+
+
+
+
